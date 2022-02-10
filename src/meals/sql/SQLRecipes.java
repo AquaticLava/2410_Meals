b29d8dd4905bfcea9aefdb5ceda19b90fb45fe74
Eric R <ryttingeric@gmail.com>
2022-02-09 18:12:32 -0700

    Added SQL classes. Added doc comments to SQL classes. Added derby jar.

---
+++ src/meals/sql/SQLRecipes.java
@@ -0,0 +1,65 @@
+package meals.sql;
+
+/**
+ * Holds methods containing strings with SQL commands for
+ * manipulating the <code>Recipes</code> table.
+ *
+ * For more information database structure documentation is located at:
+ * <a href="http://www.ericrytting.com/DatabaseDocs/">Docs</a>
+ *
+ * @author Eric R
+ */
+public class SQLRecipes {
+    /**
+     * Creates a string containing SQL commands to create
+     * the <code>Recipes</code> table.
+     * <p>
+     * Creates the table with the columns:
+     * ID, RecipeName, RecipeInstructions, CookTime,
+     * PrepTime, RecipeDescription, and CostCategory.
+     * <p>
+     *
+     * @return a string containing the SQL commands to create the
+     * <code>Recipes</code> table.
+     */
+    public String createDatabase() {
+        return null;
+        //todo Create method!
+    }
+
+    /**
+     * Creates a string containing SQL commands to drop
+     * the <code>Recipes</code> table from the database.
+     *
+     * @return the string containing the SQL commands to drop the
+     * <code>Recipes</code> table.
+     */
+    public String dropDatabase() {
+        return null;
+        //todo Create method!
+    }
+
+    /**
+     * Creates a string containing SQL commands to pull all
+     * the information from the <code>Recipes</code> table.
+     *
+     * @return the string containing the SQL commands to pull all
+     * data from the <code>Recipes</code> table.
+     */
+    public String allDataFromDatabase() {
+        return null;
+        //todo Create method!
+    }
+
+    /**
+     * Creates a string containing SQL commands to put insert new data
+     * into the <code>Recipes</code> table.
+     *
+     * @return the string containing the SQL commands to insert new
+     * data into the <code>Recipes</code> table.
+     */
+    public String insertDataIntoDatabase() {
+        return null;
+        //todo Create method!
+    }
+}
---
+++ src/meals/sql/SQLRecipesIngredients.java
@@ -0,0 +1,64 @@
+package meals.sql;
+
+/**
+ * Holds methods containing strings with SQL commands for
+ * manipulating the <code>RecipesIngredients</code> table.
+ *
+ * For more information database structure documentation is located at:
+ * <a href="http://www.ericrytting.com/DatabaseDocs/">Docs</a>
+ *
+ * @author Eric R
+ */
+public class SQLRecipesIngredients {
+    /**
+     * Creates a string containing SQL commands to create
+     * the <code>RecipesIngredients</code> table.
+     * <p>
+     * Creates the table with the columns:
+     * RecipeID and IngredientID.
+     * <p>
+     *
+     * @return a string containing the SQL commands to create the
+     * <code>RecipesIngredients</code> table.
+     */
+    public String createDatabase() {
+        return null;
+        //todo Create method!
+    }
+
+    /**
+     * Creates a string containing SQL commands to drop
+     * the <code>RecipesIngredients</code> table from the database.
+     *
+     * @return the string containing the SQL commands to drop the
+     * <code>RecipesIngredients</code> table.
+     */
+    public String dropDatabase() {
+        return null;
+        //todo Create method!
+    }
+
+    /**
+     * Creates a string containing SQL commands to pull all
+     * the information from the <code>RecipesIngredients</code> table.
+     *
+     * @return the string containing the SQL commands to pull all
+     * data from the <code>RecipesIngredients</code> table.
+     */
+    public String allDataFromDatabase() {
+        return null;
+        //todo Create method!
+    }
+
+    /**
+     * Creates a string containing SQL commands to put insert new data
+     * into the <code>RecipesIngredients</code> table.
+     *
+     * @return the string containing the SQL commands to insert new
+     * data into the <code>RecipesIngredients</code> table.
+     */
+    public String insertDataIntoDatabase() {
+        return null;
+        //todo Create method!
+    }
+}
