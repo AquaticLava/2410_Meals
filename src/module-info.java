f19b64c5d32822a447799c374907c4241649ac82
Malcolm Bailey <malcolmhbailey@gmail.com>
2022-02-15 14:57:36 -0700

    Updated module-info to work after breaking it with the new packages

--- src/module-info.java
+++ src/module-info.java
@@ -1,9 +1,12 @@
-module 2410_Meals {
+module Recipe_Book {
 	requires javafx.controls;
 	requires java.sql;
 	requires javafx.fxml;
 	requires javafx.graphics;
 	requires javafx.base;
-	
+
 	opens application to javafx.graphics, javafx.fxml;
+	opens MainMenu to javafx.graphics, javafx.fxml;
+	opens MealBrowser to javafx.graphics, javafx.fxml;
+	opens EditData to javafx.graphics, javafx.fxml;
 }
