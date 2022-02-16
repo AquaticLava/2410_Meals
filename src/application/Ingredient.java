df83644a8c13769e749a4f181ffa2e63238ccd27
CoWilll <hondpawns22@gmail.com>
2022-02-16 02:06:12 -0700

    Added Getters and Setters

--- src/application/Ingredient.java
+++ src/application/Ingredient.java
@@ -8,4 +8,28 @@ public class Ingredient {
 	private int Id;
 	private String ingredientName;
 	private String nutritionInfo;
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getIngredientName() {
+		return ingredientName;
+	}
+
+	public void setIngredientName(String ingredientName) {
+		this.ingredientName = ingredientName;
+	}
+
+	public String getNutritionInfo() {
+		return nutritionInfo;
+	}
+
+	public void setNutritionInfo(String nutritionInfo) {
+		this.nutritionInfo = nutritionInfo;
+	}
 }
